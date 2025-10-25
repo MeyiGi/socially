3efd8dab31d003603c43f13003ae5873ecea70c8
MeyiGi <faybmi7@gmail.com>
2025-10-25 13:28:07 +0600

    configured clerk - authentication feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <SignedOut>
+        <SignInButton mode="modal"/>
+        <SignUpButton mode="modal">
+          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </>
   );
 }
