fc3159bab384bc9db562950b9d3fc1fdaa384ee4
MeyiGi <faybmi7@gmail.com>
2025-10-26 13:50:39 +0600

    configured shadcn & added dark white mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import ModeToggle from '@/components/ModeToggle'
+import React from 'react'
 
-export default function Home() {
+
+function page() {
   return (
-    <>
-      <SignedOut>
-        <SignInButton mode="modal"/>
-        <SignUpButton mode="modal">
-          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-            Sign Up
-          </button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </>
-  );
+    <div>
+            <ModeToggle />
+      
+    </div>
+  )
 }
+
+export default page
