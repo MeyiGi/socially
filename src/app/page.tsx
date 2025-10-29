2c51179fd01aea9d0298c57a07e03819a2e17218
MeyiGi <faybmi7@gmail.com>
2025-10-29 18:51:23 +0600

    Desktop Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import ModeToggle from '@/components/ModeToggle'
-import React from 'react'
-
+import ModeToggle from "@/components/ModeToggle";
+import React from "react";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 function page() {
   return (
     <div>
-            <ModeToggle />
-      
+      <h1>Welcome to the Socially App!</h1>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
